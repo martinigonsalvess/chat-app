f441d5c10c553d29e862569ce4ec28970a73b9d4
Martin <martinigonsalves@gmail.com>
2020-10-13 15:08:48 +0200

    upload the chat app

---
+++ src/components/Contact.js
@@ -0,0 +1,22 @@
+import React from 'react'
+function Contact () {
+    return(
+    <div className="Contact">
+        <img className="avatar" src="https://laughingsquid.com/wp-content/uploads/tumblr_m3kqhs4OHy1qf8rjmo1_1280.jpg"
+        alt="user-pic" />
+        <div>
+            <p className="name">Iron Man</p>
+            <div className="status">
+            <div className="status-online"></div>
+            <div className="status-text">online</div>
+            </div>
+        </div>
+    </div>
+    )
+}
+export default Contact
+
+
+
+
+
---
+++ src/components/ContactTest.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+
+
+const name = "John the Boy";
+const avatar = "https://laughingsquid.com/wp-content/uploads/tumblr_m3kqhs4OHy1qf8rjmo1_1280.jpg";
+const isOnline = true;
+
+class Contact extends React.Component {
+
+    render() {
+        return (
+            <div className="Contact">
+                {<img className="avatar" src={avatar} alt="John"></img>}
+
+                <div className="name"><b>{name}</b>
+                    <div className="status">
+                        <div className="status-text">
+
+                            {isOnline ? <div className="status-online">online</div> :
+                                <div className="status-offline">offline"</div>}
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+        )
+    }
+}
+export default Contact;
